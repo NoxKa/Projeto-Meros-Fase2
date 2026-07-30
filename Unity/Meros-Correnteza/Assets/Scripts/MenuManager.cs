43fe9e01c563b15467417252d4f48c98f148f9da
NoxKa <20241ctb0180015@estudantes.ifpr.edu.br>
2026-07-30 11:07:09 -0300

    Menu

--- Unity/Meros-Correnteza/Assets/Scripts/MenuManager.cs
+++ Unity/Meros-Correnteza/Assets/Scripts/MenuManager.cs
@@ -1,10 +1,24 @@
+using System.Runtime.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject menuMain;
+
+
     public void Jogar()
     {
         SceneManager.LoadScene("Fase1"); // Inicia fase 1
+    }
+    public void  OnMenu(GameObject menu)
+    {
+        menuMain.SetActive(false);
+        menu.SetActive(true);
+    }
+    public void offMenu(GameObject menu)
+    {
+        menu.SetActive(false);
+        menuMain.SetActive(true);
     }
 }
