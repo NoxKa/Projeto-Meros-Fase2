253ce6ef1030ed6803beb51a14bf20cb585ce9dc
unknown <20241ctb0180015@estudantes.ifpr.edu.br>
2026-09-10 10:02:37 -0300

    Agua paralax

--- Unity/Meros-Correnteza/Assets/Scripts/RepeatAgua.cs
+++ Unity/Meros-Correnteza/Assets/Scripts/RepeatAgua.cs
@@ -4,18 +4,26 @@ public class RepeatAgua : MonoBehaviour
 {
     private float screenWidth;
     private float startPositionX;
-    void Start()
+    private float largura;
+    void Awake()
     {
         startPositionX = transform.position.x;
-        screenWidth = Camera.main.orthographicSize * 2f * Camera.main.aspect;
+        
+    }
+    void Start()
+    {
+        //screenWidth = Camera.main.orthographicSize * 2f * Camera.main.aspect;
+        largura = GetComponent<SpriteRenderer>().bounds.size.x;
         Debug.Log(screenWidth);
+        Debug.Log(startPositionX);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPositionX-screenWidth)
+        if (transform.position.x < startPositionX-largura/3)
         {
+            Debug.Log(transform.position.x);
             transform.position = new Vector2(startPositionX, transform.position.y);
             Debug.Log("Repeat");
         }
